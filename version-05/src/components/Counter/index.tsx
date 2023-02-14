98bcacc72fd045acb19120f93b4b04a2d7698f38
jJan fFrnando Gómez Maldonado <difex01@gmail.com>
2023-02-13 22:16:17 -0600

    Component fix

--- version-05/src/components/Counter/index.tsx
+++ version-05/src/components/Counter/index.tsx
@@ -1,41 +1,45 @@
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import './style.css'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./style.css";
 
 interface CounterProps {
-  title?: string;
+	title?: string;
 }
 
 function Counter(props: CounterProps) {
+	const { title } = props;
 
-  const { title } = props;
+	const [counter, setCounter] = useState(0);
 
-  const [counter, setCounter] = useState(0)
+	const increment = (n: number = 1) => {
+		setCounter(counter + n);
+	};
 
-  let abc = 1;
+	const decrement = (n: number = 1) => {
+		setCounter(counter - n);
+	};
 
-  const increment = (n: number = 1) => {
-    //setCounter(counter + n)
-    console.log('increment', abc)
-    abc += 1;
-    console.log('increment>', abc)
-  }
-
-  const decrement = (n: number = 1) => {
-    setCounter(counter - n)
-  }
-
-  return (
-    <div className="counter">
-      <h2>{title}: {counter}</h2>
-      <button className='btn btn-success m-2' onClick={() => increment()}>+1</button>
-      <button className='btn btn-success m-2' onClick={() => increment(2)}>+2</button>
-      <button className='btn btn-success m-2' onClick={() => increment(10)}>+10</button>
-      <button className='btn btn-danger m-2' onClick={() => decrement()}>-1</button>
-      <hr />
-      <NavLink to='/form'>Formulario</NavLink>
-    </div>
-  )
+	return (
+		<div className="counter">
+			<h2>
+				{title}: {counter}
+			</h2>
+			<button className='btn btn-success m-2' onClick={() => increment()}>
+				+1
+			</button>
+			<button className='btn btn-success m-2' onClick={() => increment(2)}>
+				+2
+			</button>
+			<button className='btn btn-success m-2' onClick={() => increment(10)}>
+				+10
+			</button>
+			<button className='btn btn-danger m-2' onClick={() => decrement()}>
+				-1
+			</button>
+			<hr />
+			<NavLink to='/form'>Formulario</NavLink>
+		</div>
+	);
 }
 
 export default Counter;
